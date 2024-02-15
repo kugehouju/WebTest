026be58ddac9e0845d965dadbb9c0d2bc912dc48
kugehouju <kugehouju@gmail.com>
2024-02-15 14:50:22 +0900

    add start window && html fix

--- 10_CursorStalker/js/main.js
+++ 10_CursorStalker/js/main.js
@@ -1,7 +1,33 @@
-document.getElementById('startAnimation').addEventListener('click', function() {
-  // SVGコンテナを表示
-  var container = document.querySelector('.container');
-  container.style.display = 'block';
-  // アニメーション用のクラスを追加
-  container.classList.add('animate-svg');
+document.addEventListener('DOMContentLoaded', function() {
+  const stalker = document.getElementById('cursor-stalker');
+  let mouseX = 0, mouseY = 0, posX = 0, posY = 0;
+
+  function mouseMoveHandler(e) {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+  }
+
+  function animate() {
+    // カーソルの先端にカーソルストーカーを配置するために、
+    // translateの値を調整します。
+    posX += (mouseX - posX) / 10; // 追従速度を遅くするために割り算の値を調整します。
+    posY += (mouseY - posY) / 10; // ここでの割り算の値を調整することで、追従速度を変更できます。
+
+    // カーソルストーカーのサイズを考慮せずに位置を設定
+    stalker.style.left = posX + 'px';
+    stalker.style.top = posY + 'px';
+
+    requestAnimationFrame(animate);
+  }
+
+  document.addEventListener('mousemove', mouseMoveHandler);
+  animate(); // アニメーションを開始します。
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  const spans = document.querySelectorAll('.shine-text span');
+  spans.forEach((span, index) => {
+    // アニメーション適用の遅延を設定
+    span.style.animationDelay = `${index * 0.5}s`;
+  });
 });
